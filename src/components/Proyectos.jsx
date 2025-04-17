4ec65eccad99e285044ed8998ca61a4760c1bc0a
Nicolas Anzola <nicolasanzola200@gmail.com>
2025-04-17 16:20:36 +0200

    issue img principal

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -30,7 +30,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Yustii App</span>
-          <p className='text-xs'>Yustii App es una plataforma legal desarrollada en Bubble.io que centraliza la comunicación entre clientes y abogados. Integra herramientas clave como firma electrónica, gestión de plantillas legales, verificación de identidad y validación en listas oficiales del Estado colombiano. El sistema soporta múltiples roles de usuario (cliente, abogado, gestor y administrador) y fue construido con una arquitectura modular, permitiendo escalabilidad e integración de nuevos servicios.</p>
+          <p className='text-sm'>Yustii App es una plataforma legal desarrollada en Bubble.io que centraliza la comunicación entre clientes y abogados. Integra herramientas clave como firma electrónica, gestión de plantillas legales, verificación de identidad y validación en listas oficiales del Estado colombiano. El sistema soporta múltiples roles de usuario (cliente, abogado, gestor y administrador) y fue construido con una arquitectura modular, permitiendo escalabilidad e integración de nuevos servicios.</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='https://apps.yustii.com/'>Demo</a></div>
             
@@ -41,7 +41,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Sistema de Gestión Interna de Ventas y Facturación</span>
-          <p className='text-xs'>Este proyecto fue desarrollado principalmente con Bubble.io y consiste en un sistema interno diseñado para gestionar ventas, facturación y pedidos. Su objetivo es optimizar los procesos operativos, brindando una solución personalizada y eficiente para el control y seguimiento de las actividades comerciales.</p>
+          <p className='text-sm'>Este proyecto fue desarrollado principalmente con Bubble.io y consiste en un sistema interno diseñado para gestionar ventas, facturación y pedidos. Su objetivo es optimizar los procesos operativos, brindando una solución personalizada y eficiente para el control y seguimiento de las actividades comerciales.</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='https://demo-sistemainterno.bubbleapps.io/version-test/home'>Demo</a></div>
             
@@ -52,7 +52,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Tienda E-commerce</span>
-          <p className='text-xs'>Este proyecto es una tienda e-commerce desarrollada en WordPress, con un diseño de interfaz realizado en Elementor y la gestión del backend a través de WooCommerce. Se trata de un proyecto real, creado a partir de un análisis detallado de los requerimientos del cliente, eligiendo las tecnologías más adecuadas para satisfacer sus necesidades y garantizar una solución eficiente y funcional.</p>
+          <p className='text-sm'>Este proyecto es una tienda e-commerce desarrollada en WordPress, con un diseño de interfaz realizado en Elementor y la gestión del backend a través de WooCommerce. Se trata de un proyecto real, creado a partir de un análisis detallado de los requerimientos del cliente, eligiendo las tecnologías más adecuadas para satisfacer sus necesidades y garantizar una solución eficiente y funcional.</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='https://amorcrochet.co'>Demo</a></div>
             
@@ -63,7 +63,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Portafolio personal</span>
-          <p className='text-xs'>Este es mi portafolio personal, desarrollado con React y Tailwind CSS. Fue uno de mis primeros proyectos y está diseñado para ser completamente responsive. Me basé parcialmente en la interfaz de Twitter para lograr un diseño limpio, moderno y fácil de navegar. Su objetivo es presentar mis habilidades, experiencia y proyectos de manera clara y visualmente atractiva, aplicando buenas prácticas de desarrollo frontend.</p>
+          <p className='text-sm'>Este es mi portafolio personal, desarrollado con React y Tailwind CSS. Fue uno de mis primeros proyectos y está diseñado para ser completamente responsive. Me basé parcialmente en la interfaz de Twitter para lograr un diseño limpio, moderno y fácil de navegar. Su objetivo es presentar mis habilidades, experiencia y proyectos de manera clara y visualmente atractiva, aplicando buenas prácticas de desarrollo frontend.</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='#'>Demo</a></div>
           </div>
@@ -75,7 +75,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Página de peliculas</span>
-          <p className='text-xs'>Este proyecto es una aplicación web sencilla de películas, desarrollada en React. Se conecta a una API pública para recopilar, filtrar y mostrar información en tiempo real sobre distintas películas. Además, incorpora librerías como react-router-dom para la gestión de rutas y una mejor experiencia de navegación entre vistas.</p>
+          <p className='text-sm'>Este proyecto es una aplicación web sencilla de películas, desarrollada en React. Se conecta a una API pública para recopilar, filtrar y mostrar información en tiempo real sobre distintas películas. Además, incorpora librerías como react-router-dom para la gestión de rutas y una mejor experiencia de navegación entre vistas.</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='https://consumo-api-peliculas.vercel.app'>Demo</a></div>
           </div>
@@ -85,7 +85,7 @@ export class Proyectos extends Component {
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='  h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
           <span>Calculadora en react</span>
-          <p className='text-xs'>Este proyecto, realizado en 2022, fue mi primer acercamiento a React. Me permitió aprender los conceptos básicos del framework y sentó las bases para desarrollar aplicaciones más complejas. A través de este proyecto, pude explorar la estructura de componentes y el manejo del estado</p>
+          <p className='text-sm'>Este proyecto, realizado en 2022, fue mi primer acercamiento a React. Me permitió aprender los conceptos básicos del framework y sentó las bases para desarrollar aplicaciones más complejas. A través de este proyecto, pude explorar la estructura de componentes y el manejo del estado</p>
           <div className='flex flex-row gap-2'>
             <div className='flex w-auto text-xs items-center text-slate-500  gap-1 hover:text-slate-300'><IoBulb /><a target="_blank" href='#'>Demo</a></div>
           </div>
--- src/components/Sobremi.jsx
+++ src/components/Sobremi.jsx
@@ -14,7 +14,7 @@ export class Sobremi extends Component {
       <div className='flex flex-col gap-5'>
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
           <div className='flex flex-row gap-2 font-bold'><img className='h-8 rounded-full border-1' src={FotoPerfil}/> <span> Nicolas Anzola</span></div>
-          <p>Soy Nicolás Anzola, tengo 24 años y me apasiona la tecnología. En 2023, obtuve el título de Ingeniero de Sistemas en la Universidad Uniminuto de Colombia. Poseo más de 3 años de experiencia en diversos roles, incluyendo desarrollador front-end, Senior low code developer, webmaster y desarrollador web. Tengo conocimientos en tecnologías como <b> HTML, CSS, JavaScript, ReactJS, NextJs, Tailwind, Bubble.io, Elementor, Wordpress y API-Rest</b>.</p>
+          <p className='text-sm'>Soy Nicolás Anzola, tengo 24 años y me apasiona la tecnología. En 2023, obtuve el título de Ingeniero de Sistemas en la Universidad Uniminuto de Colombia. Poseo más de 3 años de experiencia en diversos roles, incluyendo desarrollador front-end, Senior low code developer, webmaster y desarrollador web. Tengo conocimientos en tecnologías como <b>ReactJS, NextJs, Tailwind, HTML, CSS, JavaScript, Bubble.io, Elementor, Wordpress y API-Rest</b>.</p>
           <img className='  w-full  border-1' src={Grado}/>
         </div>
         <div className='proyectosPrincipal p-2 rounded text-slate-300 flex flex-col gap-2'>
